179d7dbd540ce5b16a2d51b7a8c2c1d65a973728
Marliece <180341631+Marliece@users.noreply.github.com>
2024-09-06 17:16:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+print("Animal Sounds")
+print()
+exit = ""
+while exit != "yes":
+  print()
+  animal = input("What animal do you want? ")
+  if animal == "cow":
+    print("A cow goes moo.")
+  elif animal == "dog":
+    print("A dog goes woof.")
+  elif animal == "cat":
+    print("A cat goes meow.")
+  elif animal == "bird":
+    print("A bird goes chirp.")
+  elif animal == "horse":
+    print("A horse goes neigh.")
+  elif animal == "sheep":
+    print("A sheep goes baa.")
+  elif animal == "pig":
+    print("A pig goes oink.")
+  elif animal == "chicken":
+    print("A chicken goes cluck.")
+  elif animal == "duck":
+    print("A duck goes quack.")
+  elif animal == "goat":
+    print("A goat goes baa.")
+  elif animal == "lion":
+    print("A lion goes roar.")
+  elif animal == "tiger":
+    print("A tiger goes roar.")
+  elif animal == "bear":
+    print("A bear goes roar.")
+  elif animal == "wolf":
+    print("A wolf goes howl.")
+  elif animal == "fox":
+    print("A fox goes howl.")
+  elif animal == "elephant":
+    print("An elephant goes trumpet.")
+  elif animal == "giraffe":
+    print("A giraffe goes hum.")
+  elif animal == "monkey":
+    print("A monkey goes ooh ooh     ah ah.")
+  elif animal == "hawk":
+    print("A hawk goes caw.")
+  elif animal == "owl":
+    print("An owl goes hoot.")
+  elif animal == "baboon":
+    print("A baboon goes ooh ooh ah ah.")
+  elif animal == "snake":
+    print("A snake goes hiss.")
+  elif animal == "bobcat":
+    print("A bobcat goes meow.")
+  elif animal == "coyote":
+    print("A coyote goes bark.")
+  elif animal == "hippopotamus":
+    print("A hippopotamus goes grunt.")
+  elif animal == "rhino":
+    print("A rhino goes grunt.")
+  elif animal == "lemur":
+    print("A lemur goes grunt.")
+  elif animal == "spider":
+    print("A spider goes hiss.")
+  elif animal == "bee":
+    print("A bee goes buzz.")
+  elif animal == "spider monkey":
+    print("A spider monkey goes grunt.")
+  elif animal == "rooster":
+    print("A rooster goes cock-a-doodle-doo.")
+  else:
+    print("I don't know that animal.")
+  print()
+  exit = input("Do you want to exit? ")
